125a87799450c0da34fc74b1569d1ae93eb00407
= <dagalloso@uc.cl>
2019-04-20 10:16:03 -0400

    Settings

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,23 @@
+module Accessible
+    extend ActiveSupport::Concern
+    included do
+        before_action :check_user
+    end
+  
+    protected
+    def check_user
+        if current_donot
+            flash.clear
+            # if you have rails_admin. You can redirect anywhere really
+            redirect_to(authenticated_donor_root_path) && return
+        elsif current_company
+            flash.clear
+            # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+            redirect_to(authenticated_company_root_path) && return
+        elsif current_socialcompany
+            flash.clear
+            # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+            redirect_to(authenticated_socialcompany_root_path) && return
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :socialcompanies, path: 'sc'
-  devise_for :companies, path: 'company'
-  devise_for :donors, path: 'donor'
+  devise_for :socialcompanies, path: 'sc', controllers: {sessions: 'sc/sessions'}
+  devise_for :companies, path: 'company', controllers: {sessions: 'company/sessions'}
+  devise_for :donors, path: 'donor', controllers: {sessions: 'donor/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

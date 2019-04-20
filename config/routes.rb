427edd9904d9b2aabf1d27ed605e0989a5b837ef
= <dagalloso@uc.cl>
2019-04-19 23:33:23 -0400

    Social companies, companies and donors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :socialcompanies, path: 'sc'
+  devise_for :companies, path: 'company'
+  devise_for :donors, path: 'donor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

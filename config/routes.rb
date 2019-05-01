dafd6c5dd2fe75853d2dafe0cc8defc4415c5f41
rfenzo <rfenzo@uc.cl>
2019-05-01 00:39:28 -0400

    translations, only login translations left

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  devise_for :socialcompanies, path: 'sc', controllers: {sessions: 'socialcompanies/sessions', registrations: 'socialcompanies/registrations'}
-  devise_for :companies, path: 'company', controllers: {sessions: 'companies/sessions', registrations: 'companies/registrations'}
-  devise_for :donors, path: 'donor', controllers: {sessions: 'donors/sessions', registrations: 'donors/registrations'}
+  devise_for :socialcompanies, path: 'sc', controllers: {registrations: 'socialcompanies/registrations'}
+  devise_for :companies, path: 'company', controllers: {registrations: 'companies/registrations'}
+  devise_for :donors, path: 'donor', controllers: {registrations: 'donors/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', to: 'session#login', as: 'login_path'
   post '/login/validate', to: 'session#validate', as: 'login_validation_path'

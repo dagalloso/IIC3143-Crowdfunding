813af5dc4fb1c5ce3f2bbb9624716368cffb22f5
= <dagalloso@uc.cl>
2019-05-01 12:23:17 -0400

    RUT added to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190422025354) do
+ActiveRecord::Schema.define(version: 20190501161848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20190422025354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
+    t.string "rut", null: false
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+    t.index ["rut"], name: "index_companies_on_rut", unique: true
   end
 
   create_table "donors", force: :cascade do |t|
